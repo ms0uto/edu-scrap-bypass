4731ed75a965a4edc10a467dee0ccbbefa315c68
ms0uto <martin_18c@hotmail.com>
2020-11-23 18:30:33 +0100

    None

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-import json, os
+import json
+import os
 import pytesseract
 from PIL import Image
-from requests_html import HTMLSession,requests
+from requests_html import HTMLSession, requests
 session = HTMLSession()
 
 
@@ -12,27 +13,32 @@ url = 'https://www.edu.xunta.es/centroseducativos/CargarDetalleCentro.do?codigo=
 
 
 def init():
-    
+
     print('Starting...')
     f = open('centros.json')
     centers = json.load(f)
     with open('emails', 'a') as emails:
         for center in centers:
-            if 'A Coru' in center['label'] and 'IES' in center['label']: # TODO modify filter.
+            label = center['label']
+            if 'A Coru' in label and 'IES' in label or 'CPR' in label or 'CPI' in label:
                 id = center['codigo']
                 r = session.get(url + id)
-                print('Retrieving center id ' + id + ' email information...')
-                r = requests.get(domain + r.html.search('img src="{}"')[0])
-                with open(id + '.png', 'wb') as f:
-                    f.write(r.content)
-                # Image recognition
-                image = Image.open(id + '.png')
-                email = pytesseract.image_to_string(image, lang='eng')
-                emails.write(email)
-                
+                image_path = r.html.search('img src="{}"')[0]
+                if image_path is not None:
+                    print('Retrieving center id ' + id + ' email information...')
+                    r = requests.get(domain)
+                    with open(id + '.png', 'wb') as f:
+                        f.write(r.content)
+                    # Image recognition
+                    image=Image.open(id + '.png')
+                    email=pytesseract.image_to_string(image, lang='eng')
+                    emails.write(email)
+                else:
+                    print('Image link not found, skipping center:' + id)
+
     os.system('rm *.png')
     print('Finished, results at: "emails" file')
-            
 
-if __name__ == "__main__" :
-     init()  
+
+if __name__ == "__main__":
+    init()
